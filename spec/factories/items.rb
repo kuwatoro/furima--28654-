cdf23ea17d96de826c98d4c94b47b9cb9a14f78e
kuwatoro <rx.1.13ayumu@icloud.com>
2020-12-22 15:14:18 +0900

    商品出品機能の修正１

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name               {Faker::Commerce.name}
     goods_explain      {Faker::Lorem.sentence}
-    category_id        {"2"}
-    goods_condition_id {"2"}
-    shipping_fee_id    {"2"}
-    prefecture_id      {"2"}
-    delivery_time_id   {"2"}
-    price              {"32000"}
+    category_id        {2}
+    goods_condition_id {2}
+    shipping_fee_id    {2}
+    prefecture_id      {2}
+    delivery_time_id   {2}
+    price              {32000}
     association :user 
 
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -31,32 +31,57 @@ describe User do
         @item.valid?
         expect(@item.errors.full_messages).to include("Goods explain can't be blank")
       end
-      it "category_idが1（空の状態）だと登録できない" do
+      it "category_idが空だと登録できない" do
         @item.category_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Category is not a number")
       end
-      it "goods_condition_idが1（空の状態）だと登録できない" do
+      it "category_idが1（空の状態）だと登録できない" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it "goods_condition_idが空だと登録できない" do
          @item.goods_condition_id = ""
          @item.valid?
         expect(@item.errors.full_messages).to include("Goods condition is not a number")
       end
-      it "shipping_fee_idが1（空の状態）だと登録できない" do
+      it "goods_condition_idが1（空の状態）だと登録できない" do
+        @item.goods_condition_id = 1
+        @item.valid?
+       expect(@item.errors.full_messages).to include("Goods condition must be other than 1")
+     end
+      it "shipping_fee_idが空だと登録できない" do
         @item.shipping_fee_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee is not a number")
       end
-      it "prefecture_idが1（空の状態）だと登録できない" do
+      it "shipping_fee_idが1（空の状態）だと登録できない" do
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee must be other than 1")
+      end
+      it "prefecture_idが空だと登録できない" do
         @item.prefecture_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture is not a number")
       end
-      it "delivery_time_idが1（空の状態）だと登録できない" do
+      it "prefecture_idが1（空の状態）だと登録できない" do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it "delivery_time_idが空だと登録できない" do
         @item.delivery_time_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery time is not a number")
       end
-      it "priceが1（空の状態）だと登録できない" do
+      it "delivery_time_idが1（空の状態）だと登録できない" do
+        @item.delivery_time_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time must be other than 1")
+      end
+      it "priceが空だと登録できない" do
         @item.price = ""
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
