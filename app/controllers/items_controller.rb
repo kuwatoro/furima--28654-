d9bcc7539746ab50d4a981ea055b372dbbba04b7
kuwatoro <rx.1.13ayumu@icloud.com>
2020-12-27 19:56:42 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,9 +20,11 @@ class ItemsController < ApplicationController
   end
 
   def show
+    #@item = Item.find(params[:id])
   end
 
   def edit
+    #@item = Item.find(params[:id])
     if current_user.id != @item.user_id
       redirect_to root_path
     end
@@ -32,6 +34,7 @@ class ItemsController < ApplicationController
   end
 
   def update
+    #@item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
       #updateアクションでは、商品情報の更新の成功失敗で分岐処理をしていただければと存じます。
@@ -39,6 +42,12 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    #@item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
   end
 
   private
